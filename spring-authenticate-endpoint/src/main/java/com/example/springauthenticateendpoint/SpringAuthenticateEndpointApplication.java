112d6342be35b2a2275a1c833ed1c5d507e0786e
kushal sharma <kushal0074@users.noreply.github.com>
2020-09-17 13:25:01 +0530

    Demo commit by kushal

--- spring-authenticate-endpoint/src/main/java/com/example/springauthenticateendpoint/SpringAuthenticateEndpointApplication.java
+++ spring-authenticate-endpoint/src/main/java/com/example/springauthenticateendpoint/SpringAuthenticateEndpointApplication.java
@@ -2,7 +2,7 @@ package com.example.springauthenticateendpoint;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//demo push by kushal at 1:20
+//demo push by kushal
 @SpringBootApplication
 public class SpringAuthenticateEndpointApplication {
 
